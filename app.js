73d900ea0a0d207eecfefba6583c42b278e0fffa
trevor <me@trevorbehnke.com>
2021-07-28 21:03:48 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,49 @@
+const chalk = require("chalk");
+const yargs = require("yargs");
+const notes = require("./notes.js");
+
+yargs.version("1.1.0");
+
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  builder: {
+    title: {
+      describe: "Note Title",
+      demandOption: true,
+      type: "string",
+    },
+    body: {
+      describe: "Note Body",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    console.log("Title: " + argv.title);
+    console.log("Body: " + argv.body);
+  },
+});
+yargs.command({
+  command: "remove",
+  describe: "Remove an old note",
+  handler: function () {
+    console.log("Removing old note!");
+  },
+});
+yargs.command({
+  command: "list",
+  describe: "Listing all notes",
+  handler: function () {
+    console.log("Listing all notes!");
+  },
+});
+yargs.command({
+  command: "read",
+  describe: "Reading a note",
+  handler: function () {
+    console.log("Reading a note!");
+  },
+});
+
+yargs.parse();
---
+++ notes.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+
+const getNotes = function () {
+  return "Your Notes...";
+};
+
+const addNotes = function (title, body) {};
+
+const loadNotes = function () {};
+
+module.exports = {
+  getNotes: getNotes,
+  addNote: addNotes,
+};
